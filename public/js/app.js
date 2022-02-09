818ed48d35f67a4a02336f04173ea2a4945a2637
Jesus Martin <jesusmarzor28@gmail.com>
2022-02-09 17:14:29 +0100

    Ajustar el tamaño del dinero de la cartera

--- public/js/app.js
+++ public/js/app.js
@@ -642,7 +642,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, ".container__money {\n  text-align: center;\n  margin: 1rem;\n  font-size: 5rem;\n}\n\n@media (max-width: 230px) {\n  .container__money {\n    font-size: 3rem;\n    margin: 0.7rem;\n  }\n}", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, ".container__money {\n  text-align: center;\n  margin: 1rem;\n  font-size: 4rem;\n}\n\n@media (max-width: 350px) {\n  .container__money {\n    font-size: 2rem;\n    margin: 0.7rem;\n  }\n}", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
